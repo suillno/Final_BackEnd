a6f07e46069606ddadfabe0e95d159c20d9c2784
Hankibok <song6115@naver.com>
2025-07-01 14:13:51 +0900

    찜 좋아요 백앤드 정보기능
    찜 좋아요 백앤드 정보기능

---
+++ src/main/java/kr/co/kh/service/GameMemberService.java
@@ -0,0 +1,22 @@
+package kr.co.kh.service;
+
+import kr.co.kh.model.CustomUserDetails;
+import kr.co.kh.model.vo.GameCartVO;
+import kr.co.kh.model.vo.GameReviewVO;
+import java.util.List;
+import kr.co.kh.model.vo.GameLikeVO;
+
+public interface GameMemberService {
+    // 리뷰 저장
+    boolean reviewSave(GameReviewVO vo);
+    // 리뷰리스트 조회
+    List<GameReviewVO> reviewListByGameId(Long gameId);
+    // 프로시저사용 찜
+    String toggleGameLike(GameLikeVO vo);
+    // 프로시저사용 장바구니
+    String toggleGameCart(GameCartVO vo);
+
+    boolean checkLike(Long gameId, CustomUserDetails user);
+    boolean checkCart(Long gameId, CustomUserDetails user);
+
+}
