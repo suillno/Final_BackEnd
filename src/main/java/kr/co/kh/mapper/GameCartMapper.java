26ace4e757eb742fe14130c1c68cb38bdd1e72fc
Hankibok <song6115@naver.com>
2025-07-15 11:39:11 +0900

    라이브러리 추가
    라이브러리 추가

--- src/main/java/kr/co/kh/mapper/GameCartMapper.java
+++ src/main/java/kr/co/kh/mapper/GameCartMapper.java
@@ -1,6 +1,7 @@
 package kr.co.kh.mapper;
 
 import kr.co.kh.model.vo.GameCartVO;
+import kr.co.kh.model.vo.GameLibraryVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -15,5 +16,6 @@ public interface GameCartMapper {
     List<GameCartVO> getCartByUser(String userName);
     // 카트 상태체크
     int checkCart(GameCartVO vo);
-    
+    // 라이브러리 조회
+    List<GameLibraryVO> getAllLibraryByUser(String userName);
 }
